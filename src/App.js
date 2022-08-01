33810b42c667141e1b109936c2540e5203d63025
Truong Phuc Hung <hungtp@vnextglobal.com>
2022-08-01 14:12:29 +0700

    dvd logo init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+import { useWindowSize } from './hooks/useWindowSize'
 
 function App() {
+  const [top, setTop] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [isEnd, setIsEnd] = useState(false)
+  const size = useWindowSize()
+  useEffect(() => {
+    let leftInterval;
+    let rollbackInterval;
+    let topInterval;
+   
+
+    if (left === size.width - 60) {
+      setIsEnd(true)
+    }
+
+    if (left === 0) {
+      setIsEnd(false)
+    }
+
+    if (!isEnd) {
+      leftInterval = setInterval(() => {
+        setLeft(prev => prev + 10)
+      })
+    } else {
+      rollbackInterval = setInterval(() => {
+        setLeft(prev => prev - 10)
+      })
+    }
+    // } else {
+    //   if (top < size.height - 60) {
+    //     topInterval = setInterval(() => {
+    //       setTop(prev => prev + 10)
+    //     }, 10)
+    //   }
+    //   console.log(left)
+    // }
+    return () => {
+      clearInterval(leftInterval)
+      clearInterval(topInterval)
+      clearInterval(rollbackInterval)
+    }
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Circle top={`${top}px`} left={`${left}px`} />
     </div>
   );
 }
 
 export default App;
+
+const Circle = styled.div`
+  height: 50px;
+  width: 50px;
+  background-color: lightblue;
+  border-radius: 50%;
+  position: absolute;
+  top: ${props => props.top};
+  left: ${props => props.left}
+`
